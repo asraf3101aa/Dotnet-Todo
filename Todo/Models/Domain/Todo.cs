b59e0566b95e7139a6cec028b51454e0352bfe2f
Asraf Ansari <asraf3101aa@gmail.com>
2023-05-15 07:11:56 +0545

    Properties defined for Todo domain model

---
+++ Todo/Models/Domain/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace Todo.Models.Domain
+{
+    public class Todo
+    {
+        public Guid Id { get; set; }
+        public string Task { get; set; }
+        public DateOnly Date { get; set; }
+        public Boolean Complete { get; set; }
+        public string Description { get; set; }
+    }
+}
